79bddc854b6e2833a5c7e4cb11348a9e2bc4100e
Krissy Lewis <kristianlewis03@gmail.com>
2019-10-22 21:54:29 +0000

    create and test authenticate_with_credentials method

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,12 @@ class User < ActiveRecord::Base
   validates :last_name, presence: true
   validates :email, presence: true, :uniqueness => {:case_sensitive => false}
   validates :password, presence: true, :length => {:minimum => 5}
+
+  def authenticate_with_credentials(email, password)
+    user_email = email.downcase.strip
+    user = User.find_by_email(user_email)
+
+    user && user.authenticate(password) ? user : nil
+  end
+
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,28 +4,61 @@ RSpec.describe User, type: :model do
   before(:each) do
     @user = User.new(first_name: 'First', last_name: 'Name', email: 'email@email.com', password: 'password', password_confirmation: 'password')
   end
+  describe 'Validations' do
+    it 'returns valid if all parameters exist' do
+      expect(@user).to be_valid
+    end
 
-  it 'returns valid if all parameters exist' do
-    expect(@user).to be_valid
+    it "should be invalid if password and password_confirmation don't match" do
+      @user.password = 'passw'
+      @user.password_confirmation = 'pword'
+
+      expect(@user).to_not be_valid
+    end
+
+    it 'should be invalid if email is not unique' do
+      user = User.create!(first_name: 'Existing', last_name: 'User', email: 'EMAIL@email.COM', password: 'pword', password_confirmation: 'pword')
+
+      expect(@user).to_not be_valid
+    end
+
+    it 'should be invalid of password does not meet the minimum length of 5 characters' do
+      @user.password = 'pass'
+      @user.password_confirmation = 'pass'
+
+      expect(@user).to_not be_valid
+    end
+
   end
 
-  it "should be invalid if password and password_confirmation don't match" do
-    @user.password = 'passw'
-    @user.password_confirmation = 'pword'
+  describe '.authenticate_with_credentials' do
+    it 'should login in user with successful credentials' do
+      @user.save
+      authentic = @user.authenticate_with_credentials(@user.email, @user.password)
 
-    expect(@user).to_not be_valid
+      expect(authentic).to eql(@user)
+    end
+
+    it 'should not login in user with invalid credentials' do
+      @user.save
+      unsuccessful = @user.authenticate_with_credentials(@user.email, @user.password = 'somepassword')
+
+      expect(unsuccessful).to eql(nil)
+    end
+
+    it 'should login in user without email case-sensitivity' do
+      @user.save
+      authenticate_email = @user.authenticate_with_credentials(@user.email = 'EMAIL@emaIL.Com', @user.password)
+
+      expect(authenticate_email).to eql(@user)
+    end
+
+    it 'should authenticate user regardless of whitespace around email' do
+      @user.save
+      with_whitespace = @user.authenticate_with_credentials(@user.email = ' EMAIL@emaIL.Com ', @user.password)
+
+      expect(with_whitespace).to eql(@user)
+    end
   end
 
-  it 'should be invalid if email is not unique' do
-    user = User.create!(first_name: 'Existing', last_name: 'User', email: 'EMAIL@email.COM', password: 'pword', password_confirmation: 'pword')
-
-    expect(@user).to_not be_valid
-  end
-
-  it 'should be invalid of password does not meet the minimum length of 5 characters' do
-    @user.password = 'pass'
-    @user.password_confirmation = 'pass'
-
-    expect(@user).to_not be_valid
-  end
 end
